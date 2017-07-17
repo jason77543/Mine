c3dae0a53afde5454fb26b5957a3fc35bfddd099
jason <jason77543@gmail.com>
2017-07-17 15:50:05 +0800

    model&SQLAllFinish

---
+++ src/com/actDetial/model/ActDetialDAO.java
@@ -0,0 +1,37 @@
+package com.actDetial.model;
+
+import java.util.List;
+
+public class ActDetialDAO implements ActDetialDAO_Interface{
+
+	@Override
+	public void add(ActDetial actDetial) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(ActDetial actDetial) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer actNo, Integer memNo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ActDetial findByPK(Integer actNo, Integer memNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ActDetial> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
