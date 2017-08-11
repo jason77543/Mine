a20759e94de4381b8cdf9452e8a23ef8781827a4
jason <jason77543@gmail.com>
2017-08-11 18:01:01 +0800

    frontEnd2

--- src/another/TTest.java
+++ src/another/TTest.java
@@ -1,20 +1,10 @@
 package another;
-import java.awt.Button;
-import java.awt.Event.*;
-import java.awt.Dialog;
-import java.awt.FlowLayout;
-import java.awt.Frame;
-import java.awt.Label;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
+
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -23,16 +13,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.actImg.model.ActImg;
-import com.actImg.model.ActImgJDBCDAO;
-import com.activity.model.Activity;
-import com.activity.model.ActivityDAO;
-import com.activity.model.ActivityJDBCDAO;
-import com.restImg.model.RestImg;
-import com.restImg.model.RestImgDAO;
-import com.restImg.model.RestImgJDBCDAO;
 
-import javafx.scene.control.DialogEvent;
 
 		
 public class TTest {
